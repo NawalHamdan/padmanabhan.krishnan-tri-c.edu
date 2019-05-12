463c1b32a034bae5a36397933c38de94bf6764e1
NawalHamdan <48875153+NawalHamdan@users.noreply.github.com>
2019-05-12 13:08:35 -0400

    LAB2/3/5

--- Program.cs
+++ Program.cs
@@ -4,43 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab3
+namespace lab2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int Speedlimit;
-            int speed;
-
-            Speedlimit = 35;
-            speed = 42;
-
-            if (speed > Speedlimit)
-            {
-                Console.WriteLine("SlowDown!");
-            }
-
-            Console.WriteLine("Input temperature in Fahrenheit: ");
-            int fah = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-
-            int FtoC = ((fah - 32) / 9) * 5;
-            Console.WriteLine("Temperature in Celsius is {0}: ", FtoC);
-            Console.ReadLine();
-
-            int i = 1;
-
-            while (i < 11)
-            {
-                Console.Write("While Output: ");
-                Console.WriteLine(i);
-                i++;
-            }
-
         }
     }
 }
-
-
-// 2. Both if single and while repetition statements share the similarity in which they must evaluate if a given value is either true or false. Requiring they meet a certain condition in order for them to stop executing code. They are different because the “if” condition only does what it is told to, then stops. While the “while” condition repeats over and over until it hits its loop termination. 
